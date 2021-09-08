18ccaaaa21a9e4f55fad7ac2d8071602e9c86536
Adrian Camacho <adriancamacho18@gmail.com>
2021-09-08 13:41:22 -0700

    add initial files to app
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

---
+++ src/components/MerchControl.js
@@ -0,0 +1,45 @@
+import React from "react";
+import NewMerchForm from "./NewMerchForm";
+import MerchList from "./MerchList";
+
+class MerchControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      masterMerchList: []
+    };
+  }
+  
+  handleAddingNewMerchToList = (newMerch) => {
+    const newMasterMerchList = this.state.masterMerchList.concat(newMerch);
+    this.setState({
+      masterMerchList: newMasterMerchList,
+      formVisibleOnPage: false,
+    });
+  };
+
+  handleClick = () => {
+    this.setState(prevState => 
+      ({formVisibleOnPage: !prevState.formVisibleOnPage}));
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewMerchForm onNewMerchCreation={this.handleAddingNewMerchToList} />
+      buttonText = 'Return to Merch List';
+    } else {
+      currentlyVisibleState = <MerchList merchList = {this.state.masterMerchList} />
+      buttonText = 'Add Merch';
+    }
+    return (
+      <>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </>
+    )
+  }
+}
